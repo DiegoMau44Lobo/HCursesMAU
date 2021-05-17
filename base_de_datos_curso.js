186188f1c89c81f6d67ff126dcf6bb096cbcd7cc
Diego44Mau <84315426+Diego44Mau@users.noreply.github.com>
2021-05-17 12:09:22 -0500

    Update base_de_datos_curso.js

--- base_de_datos_curso.js
+++ base_de_datos_curso.js
@@ -1,7 +1,7 @@
 export const cursos = [
     {
         titulo_curso: "Master en Python Aprender Python 3, Django, Flask y Tkinter",
-        imagen_curso: "/imagenes_pagina/img-master_python.jpg",
+        imagen_curso: "https://raw.githubusercontent.com/Diego44Mau/HCurses/main/imagenes_pagina/img-master_python.jpg",
         descripcion_curso: "Aprende a programar desde cero con Python 3 y desarrollo web con los frameworks Django 3 y Flask paso a paso (+30 horas)",
         enlace_youtube: "https://youtube.com/playlist?list=PLygVLLavW39no5aYoh-MV7e59pMnh4uAm",
         enlace_archivos: "https://drive.google.com/drive/u/2/folders/1b4DiuBCYMkjMC8vR-Q3_-YTliE5OIrci"
@@ -9,7 +9,7 @@ export const cursos = [
 
     {
         titulo_curso: "Bootstrap 5! Curso completo desde 0! incluye Sass y Firebase",
-        imagen_curso: "/imagenes_pagina/img-bootstrap.jpg",
+        imagen_curso: "https://raw.githubusercontent.com/Diego44Mau/HCurses/main/imagenes_pagina/img-bootstrap.jpg",
         descripcion_curso: "Aprende a desarrollar páginas web responsives (mobile first) con Bootstrap 5 [Rápido y Fácil], El Framework más Popular!",
         enlace_youtube: "https://youtube.com/playlist?list=PLygVLLavW39kI9k29T72V95pVHL4xRz5R",
         enlace_archivos: "https://drive.google.com/drive/folders/19TxCxpDUskQ7NMW7m9Tq_c-sgkEK5ORj?usp=sharing"
@@ -17,7 +17,7 @@ export const cursos = [
 
     {
         titulo_curso: "Bases de Datos desde Cero 3",
-        imagen_curso: "/imagenes_pagina/img-bases_de_datos.png",
+        imagen_curso: "https://raw.githubusercontent.com/Diego44Mau/HCurses/main/imagenes_pagina/img-bases_de_datos.png",
         descripcion_curso: "Diseña, estructura y administra bases de datos SQL y crea un sistema de facturación",
         enlace_youtube: "https://www.youtube.com/playlist?list=PLygVLLavW39mLuDQch1JtSs7_ggO4Gn1O",
         enlace_archivos: "#"
@@ -25,7 +25,7 @@ export const cursos = [
 
     {
         titulo_curso: "Desarrollo Frontend con React.js",
-        imagen_curso: "/imagenes_pagina/img-desarrollo_front_con_react.png",
+        imagen_curso: "https://raw.githubusercontent.com/Diego44Mau/HCurses/main/imagenes_pagina/img-desarrollo_front_con_react.png",
         descripcion_curso: "En esta especialidad aprenderás lo necesario para entender desde las bases de React, pasando por las integraciones con otras librerías y extensiones hasta poder construir tu primera aplicación real tal y como fue construida la app de EDteam.",
         enlace_youtube: "https://youtube.com/playlist?list=PLygVLLavW39nuETVg8wix5o1-05ASks00",
         enlace_archivos: "https://drive.google.com/drive/folders/1EtiX7TumQGoyyME6GGksCXoXUR62n5oN?usp=sharing"
@@ -33,7 +33,7 @@ export const cursos = [
 
     {
         titulo_curso: "Animaciones CSS",
-        imagen_curso: "/imagenes_pagina/img-animaciones_css.jpg",
+        imagen_curso: "https://raw.githubusercontent.com/Diego44Mau/HCurses/main/imagenes_pagina/img-animaciones_css.jpg",
         descripcion_curso: "Crea animaciones optimizadas para la web en 2D y 3D sin programar.",
         enlace_youtube: "https://youtube.com/playlist?list=PLygVLLavW39k_EIGY2TLy-HA5aC0Z4D71",
         enlace_archivos: "https://drive.google.com/drive/folders/1-MdZiuzfTq1SRrK1u8bP5zljmxk28WC1?usp=sharing"
@@ -41,7 +41,7 @@ export const cursos = [
 
     {
         titulo_curso: "Especialidad: Diseño de interfaces (UI Design)",
-        imagen_curso: "/imagenes_pagina/img-UI.jpg",
+        imagen_curso: "https://raw.githubusercontent.com/Diego44Mau/HCurses/main/imagenes_pagina/img-UI.jpg",
         descripcion_curso: "Domina la teoría y las herramientas para convertirte en un diseñador de mockups y prototipos para web y aplicaciones.",
         enlace_youtube: "https://youtube.com/playlist?list=PLygVLLavW39kvos3QEFMle52qtH0-6eku",
         enlace_archivos: "https://drive.google.com/drive/folders/14MukEzjYBo6njWh7V7pZKLl6FIepz4Zx?usp=sharing"
@@ -49,7 +49,7 @@ export const cursos = [
 
     {
         titulo_curso: "Desarrollo Web con Django 3 desde cero",
-        imagen_curso: "/imagenes_pagina/img-django.jpg",
+        imagen_curso: "https://raw.githubusercontent.com/Diego44Mau/HCurses/main/imagenes_pagina/img-django.jpg",
         descripcion_curso: "Domina la teoría y las herramientas para convertirte en un diseñador de mockups y prototipos para web y aplicaciones.",
         enlace_youtube: "https://www.youtube.com/watch?v=5HDlgXTKVx4&list=PLygVLLavW39no5aYoh-MV7e59pMnh4uAm&index=176",
         enlace_archivos: "https://drive.google.com/drive/folders/14MukEzjYBo6njWh7V7pZKLl6FIepz4Zx?usp=sharing"
@@ -65,7 +65,7 @@ export const cursos = [
 
     {
         titulo_curso: "Bases de Datos - SQLite y MySQL",
-        imagen_curso: "/imagenes_pagina/img-sqlite.jpg",
+        imagen_curso: "https://raw.githubusercontent.com/Diego44Mau/HCurses/main/imagenes_pagina/img-sqlite.jpg",
         descripcion_curso: "Domina la teoría y las herramientas para convertirte en un diseñador de mockups y prototipos para web y aplicaciones.",
         enlace_youtube: "https://www.youtube.com/watch?v=t7OJQdR6Jm0&list=PLygVLLavW39no5aYoh-MV7e59pMnh4uAm&index=89",
         enlace_archivos: "https://drive.google.com/drive/folders/14MukEzjYBo6njWh7V7pZKLl6FIepz4Zx?usp=sharing"
@@ -73,7 +73,7 @@ export const cursos = [
 
     {
         titulo_curso: "Desarrollo web con Flask",
-        imagen_curso: "/imagenes_pagina/img-flask.jpg",
+        imagen_curso: "https://raw.githubusercontent.com/Diego44Mau/HCurses/main/imagenes_pagina/img-flask.jpg",
         descripcion_curso: "Domina la teoría y las herramientas para convertirte en un diseñador de mockups y prototipos para web y aplicaciones.",
         enlace_youtube: "https://www.youtube.com/watch?v=sbiDvWFe37k&list=PLygVLLavW39no5aYoh-MV7e59pMnh4uAm&index=295",
         enlace_archivos: "https://drive.google.com/drive/folders/14MukEzjYBo6njWh7V7pZKLl6FIepz4Zx?usp=sharing"
@@ -81,7 +81,7 @@ export const cursos = [
 
     {
         titulo_curso: "Curso de Hacking Ético",
-        imagen_curso: "/imagenes_pagina/img-hacking_etico.jpg",
+        imagen_curso: "https://raw.githubusercontent.com/Diego44Mau/HCurses/main/imagenes_pagina/img-hacking_etico.jpg",
         descripcion_curso: "Aprende técnicas y herramientas para realizar ataques informáticos a sitios web con el fin de detectar y corregir vulnerabilidades. Conviértete en un Hacker Ético y contribuye a lograr una mayor Seguridad Informática en internet con este increíble curso de Platzi.",
         enlace_youtube: "https://youtube.com/playlist?list=PLygVLLavW39mcxHe7I7im7Y0wcqs8nrJ-",
         enlace_archivos: "https://drive.google.com/drive/folders/1TZIWdn2VZdBJolb5sapV2wQoNg-NTQK5?usp=sharing"
@@ -89,7 +89,7 @@ export const cursos = [
 
     {
         titulo_curso: "Aprende SQL desde cero ¡Curso con más de 100 ejercicios!",
-        imagen_curso: "/imagenes_pagina/img-SQL.jpg",
+        imagen_curso: "https://raw.githubusercontent.com/Diego44Mau/HCurses/main/imagenes_pagina/img-SQL.jpg",
         descripcion_curso: "2021: El curso de SQL que te da más valor: Aprenderás cómo hacer consultas SQL desde básicas hasta complejas.",
         enlace_youtube: "https://youtube.com/playlist?list=PLygVLLavW39nfYadrWfS8quvjZU_XVMZl",
         enlace_archivos: "https://drive.google.com/drive/folders/1TZIWdn2VZdBJolb5sapV2wQoNg-NTQK5?usp=sharing"
